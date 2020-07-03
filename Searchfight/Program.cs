c2dc42db9bba32ced335d59bb5b57ce3cb14688c
ramesh <rameshsoftcsc@gmail.com>
2020-07-03 19:25:20 +0530

    added autofac container

--- Searchfight/Program.cs
+++ Searchfight/Program.cs
@@ -1,26 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
 namespace Searchfight
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var builder = new ContainerBuilder();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			using (var container = builder.Build())
+			{
+				await StartWebServerAsync(container);
+			}
+		}
+
+		private static async Task StartWebServerAsync(ILifetimeScope scope)
+		{
+			using (var webHostScope = scope.BeginLifetimeScope(builder => builder.RegisterType<Startup>().AsSelf()))
+			{
+				await WebHost
+					.CreateDefaultBuilder()
+					.UseStartup<Startup>()
+					.ConfigureServices(services => services.AddTransient(provider => webHostScope.Resolve<Startup>()))
+					.Build()
+					.RunAsync();
+			}
+		}
+	}
 }
--- Searchfight/Startup.cs
+++ Searchfight/Startup.cs
@@ -1,3 +1,5 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -6,26 +8,38 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SearchfightEngine.Interfaces;
 using SearchfightEngine.Services;
+using System;
 
 namespace Searchfight
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILifetimeScope _webHostScope;
+        private ILifetimeScope _aspNetScope;
+
+        public Startup(ILifetimeScope webHostScope)
         {
-            Configuration = configuration;
+            _webHostScope = webHostScope ?? throw new ArgumentNullException(nameof(webHostScope));
         }
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddControllers();
             services.AddTransient<ISearchfightSummaryCounter, SearchfightSummaryCounter>();
             services.AddTransient<ISearchService, SearchService>();
+            // doesn't work with Autofac 4.6.0 and child scopes.
+            // An exception is thrown on startup: Cannot resolve parameter 'IOptionsFactory<KestrelServerOptions>'
+            _aspNetScope = _webHostScope.BeginLifetimeScope(builder => builder.Populate(services));
+
+           
+            
+            return new AutofacServiceProvider(_aspNetScope);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, Microsoft.AspNetCore.Hosting.IApplicationLifetime appLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +56,7 @@ namespace Searchfight
             {
                 endpoints.MapControllers();
             });
+            appLifetime.ApplicationStopped.Register(() => _aspNetScope.Dispose());
         }
     }
 }
